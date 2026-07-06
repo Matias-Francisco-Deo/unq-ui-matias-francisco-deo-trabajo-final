d686d74b93036de8b6f574e8f39e422589e67dc2
matias-francisco-deo <matiasfd.deo@gmail.com>
2026-07-06 16:01:42 -0300

    sumar fuente de deltarune, inputs y botones. Layout inicial para gamepage

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,7 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function GamePage() {
   return (
-    <div className="w-full h-full">
-      <div>aasdsdas</div>
+    <div className="w-full h-full flex flex-col items-center">
+      <h1 className="text-4xl p-10">Palabras Encadenadas</h1>
+      <div className="size-1/3 flex justify-center gap-20">
+        <div className="aspect-rectangle size-1/2 border flex flex-col items-center text-xl py-4 gap-2">
+          <label className="text-sm">Palabra anterior:</label>
+          <span>Flower man</span>
+        </div>
+        <div className="aspect-square border text-sm flex flex-col items-center justify-center py-4 gap-2 ">
+          <label className="text-sm">Escribe tu palabra:</label>
+          <form
+            className=" flex flex-col items-center"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <Input
+              className="w-4/5"
+              placeholder="¡Aquí!"
+              onChange={(evt) => {
+                console.log(evt.target.value.toLowerCase());
+                if (evt.target.value.toLowerCase() === "gaster") {
+                  window.location.href = "/";
+                }
+              }}
+            ></Input>
+            <Button type="submit" className="w-4/5">
+              Enviar
+            </Button>
+          </form>
+        </div>
+        <div className="aspect-square size-1/2 border text-sm flex flex-col items-center py-4 gap-2">
+          <label className="text-sm">Puntaje:</label>
+          <label className="text-xl">1227</label>
+        </div>
+      </div>
     </div>
   );
 }
