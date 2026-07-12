7e953bad7b218fc374ea8403b583f07281eebb6f
matias-francisco-deo <matiasfd.deo@gmail.com>
2026-07-12 13:23:05 -0300

    Update Header.tsx

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -6,7 +6,7 @@ export const Header = ({
 }: React.ComponentProps<"header">) => {
   return (
     <header
-      className="border border-white w-full flex text-2xl flex-row justify-center items-center px-30 sticky top-0 h-1/3 gap-25"
+      className="border border-white w-full flex text-2xl flex-row justify-center items-center px-30 sticky top-0 h-1/3 gap-25 border-x-0"
       {...props}
     >
       <NavButton to={"game"}>Game</NavButton>
